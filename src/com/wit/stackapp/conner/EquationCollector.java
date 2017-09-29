28338b39577fd4617d56ab740e34418c68495d46
Conner Theberge <ctheberge96@gmail.com>
2017-09-29 12:47:45 -0400

    Finished Unit Testing
    Unit testing of EquationCollector is finished and can be called
    statically using doUnitTesting(). Work is completed. Text files included
    are used for unit testing.

--- src/com/wit/stackapp/conner/EquationCollector.java
+++ src/com/wit/stackapp/conner/EquationCollector.java
@@ -4,11 +4,19 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
 
+import com.wit.stackapp.main.UnitTester;
 import com.wit.stackapp.main.VectorStack;
 
 public class EquationCollector {
 
 	private VectorStack<String> equations;
+	
+	/**
+	 * Checks whether there is another equation
+	 * available in the equation stack.
+	 * 
+	 * @return Whether there is another equation available
+	 */
 	public boolean hasNextEquation() {
 		
 		if (checkInit()) {
@@ -18,6 +26,15 @@ public class EquationCollector {
 		} else { System.out.println("EQUATIONCOLLECTOR NOT INITIALIZED"); return false; }
 		
 	}
+	
+	/**
+	 * Gets the next available equation.
+	 * 
+	 * Can throw a stack exception if a get is
+	 * attempted while equation stack is empty.
+	 * 
+	 * @return The next equation
+	 */
 	public String getNextEquation() {
 		
 		if (checkInit()) {
@@ -32,15 +49,23 @@ public class EquationCollector {
 	private boolean initialized = false;
 	private boolean checkInit() { return initialized; }
 	
+	/**
+	 * Constructs a new EquationCollector, collecting equations line
+	 * by line from a file in the designated file path
+	 * 
+	 * @param filepath The path to the file to get equations from
+	 */
 	public EquationCollector(String filepath) {
 		
 		equations = new VectorStack<String>();
 		
-		if (filepath == null) { System.out.println("FILEPATH CANNOT BE NULL"); }
+		if (filepath == null) { System.out.println("FILEPATH CANNOT BE NULL"); return; }
 		
 		try {
 			
-			Scanner fileIn = new Scanner(new File(filepath));
+			File f = new File(filepath);
+			
+			Scanner fileIn = new Scanner(f);
 		
 			while (fileIn.hasNextLine()) {
 				
@@ -76,10 +101,10 @@ public class EquationCollector {
 
 		VectorStack<Character> equationChars = new VectorStack<>();
 
+		int openParens = 0;
+		int closedParens = 0;
+		
 		for (int i = 0; i < equation.length(); i++) {
-			
-			int openParens = 0;
-			int closedParens = 0;
 			
 			char nextChar = 0;
 			char prevChar = 0;
@@ -123,6 +148,8 @@ public class EquationCollector {
 			
 		}
 		
+		if (closedParens != openParens) { return false; }
+		
 		return true;
 		
 	}
@@ -162,9 +189,142 @@ public class EquationCollector {
 		
 	}
 	
+	/**
+	 * Conducts unit tests. Prints directly to the console.
+	 */
 	public static void doUnitTests() {
 		
+		testContructor();
 		
+		testValidityChecking();
+		
+	}
+
+	private static void testContructor() {
+		
+		UnitTester.printTestDescription("Getting Equations: normal file");
+		
+		EquationCollector collectorOne =  new EquationCollector("src/com/wit/stackapp/conner/test.txt");
+		
+		StringBuilder sb = new StringBuilder();
+		
+		while(collectorOne.hasNextEquation()) {
+			
+			sb.append(collectorOne.getNextEquation() + " ");
+			
+		}
+		
+		UnitTester.printResult("item6 item5 item4 item3 item2 item1 ", sb);
+		
+		
+		UnitTester.printTestDescription("Getting Equations: null filepath");
+		
+		System.out.println("Expected: FILEPATH CANNOT BE NULL");
+		
+		EquationCollector collectorTwo = new EquationCollector(null);
+		
+		UnitTester.printTestDescription("Getting Equations: collector isn't initialized");
+		System.out.println("Expected: EQUATIONCOLLECTOR NOT INITIALIZED");
+		collectorTwo.getNextEquation();
+		
+		
+		UnitTester.printTestDescription("Getting Equations: empty file");
+		EquationCollector collectorThree = new EquationCollector("src/com/wit/stackapp/conner/EmptyFile.txt");
+		
+		sb = new StringBuilder("[]");
+		
+		while(collectorThree.hasNextEquation()) {
+			
+			sb.append(collectorThree.getNextEquation() + " ");
+			
+		}
+		
+		UnitTester.printResult("[]", sb.toString());
+		
+		
+		UnitTester.printTestDescription("Getting Equations: A non-existant filepath");
+		
+		System.out.println("Expected: \tEQUATION COLLECTOR CAN'T FIND FILE: src/com/wit/stackapp/conner/unicorn.txt");
+		@SuppressWarnings("unused")
+		EquationCollector collectorFour = new EquationCollector("src/com/wit/stackapp/conner/unicorn.txt");
+		
+	}
+	
+	private static void testValidityChecking() {
+		
+		UnitTester.printTestDescription("File with only invalid expressions");
+		
+		int totalEquations = 0;
+		int invalidExpressions = 0;
+		
+		EquationCollector collectorOne = new EquationCollector("src/com/wit/stackapp/conner/invalidOnly.txt");
+		
+		while(collectorOne.hasNextEquation()) {
+			
+			totalEquations++;
+			
+			if (!isValid(collectorOne.getNextEquation())) {
+				
+				invalidExpressions++;
+				
+			}
+			
+		}
+		
+		UnitTester.printResult("Total: 9 Invalid: 9", "Total: " + Integer.toString(totalEquations) + " Invalid: " + Integer.toString(invalidExpressions));
+		
+		
+		UnitTester.printTestDescription("File with only valid expressions");
+		
+		totalEquations = 0;
+		int validExpressions = 0;
+		
+		EquationCollector collectorTwo = new EquationCollector("src/com/wit/stackapp/conner/validOnly.txt");
+		
+		while(collectorTwo.hasNextEquation()) {
+			
+			totalEquations++;
+			
+			String e = collectorTwo.getNextEquation();
+			
+			if (isValid(e)) {
+				
+				validExpressions++;
+				
+			}
+			
+		}
+		
+		UnitTester.printResult("Total: 36 Valid: 36", "Total: " + Integer.toString(totalEquations) + " Valid: " + Integer.toString(validExpressions));
+		
+		
+		UnitTester.printTestDescription("File with mixed expressions");
+		
+		totalEquations = 0;
+		validExpressions = 0;
+		invalidExpressions = 0;
+		
+		EquationCollector collectorThree = new EquationCollector("src/com/wit/stackapp/conner/mixedValidInvalid.txt");
+		
+		while(collectorThree.hasNextEquation()) {
+			
+			totalEquations++;
+			
+			String e = collectorThree.getNextEquation();
+			
+			if (isValid(e)) {
+				
+				validExpressions++;
+				
+			} else {
+				
+				invalidExpressions++;
+				
+			}
+			
+		}
+		
+		UnitTester.printResult("Total: 46 Valid: 36 Invalid: 10", "Total: " + Integer.toString(totalEquations) + " Valid: " + Integer.toString(validExpressions) + " Invalid: " + Integer.toString(invalidExpressions));
 		
 	}
 	
--- src/com/wit/stackapp/main/Main.java
+++ src/com/wit/stackapp/main/Main.java
@@ -4,23 +4,13 @@ import com.wit.stackapp.conner.EquationCollector;
 
 public class Main {
 
+	public static final String TEST_FILE_PATH = "Infix Calculator Expressions - multi-digit with invalid expressions -- 2016-10-04 01.txt";
+	
 	public static void main(String[] args) {
-		
-		//CalculatorGui.startApplication(args);
 
-		EquationCollector ec = new EquationCollector("Infix Calculator Expressions - multi-digit with invalid expressions -- 2016-10-04 01.txt");
+		//CalculatorGui.startApplication(args);
 		
-		while (ec.hasNextEquation()) {
-			
-			String equation = ec.getNextEquation();
-			
-			if (EquationCollector.isValid(equation)) {
-				
-				System.out.println(equation + " is valid");
-				
-			} else { System.out.println(equation + " is not valid"); }
-			
-		}
+		EquationCollector.doUnitTests();
 		
 	}
 
